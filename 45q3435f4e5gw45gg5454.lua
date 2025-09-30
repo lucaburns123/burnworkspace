7c23a9e02a8581d2d8d364c978af1246967df618
dwit <65109037+lucaburns123@users.noreply.github.com>
2025-10-01 00:31:05 +0100

    Update 45q3435f4e5gw45gg5454.lua

--- 45q3435f4e5gw45gg5454.lua
+++ 45q3435f4e5gw45gg5454.lua
@@ -447,9 +447,12 @@ Section4misc:AddButton({
     confirm = true,
     risky = false,
     callback = function()
-        for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.Idled)) do
-        v:Disable()
-        end
+        local vu = game:GetService("VirtualUser")
+	        game:GetService("Players").LocalPlayer.Idled:connect(function()
+		        vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+		        wait(1)
+		        vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+	        end)
     end
 })
 
